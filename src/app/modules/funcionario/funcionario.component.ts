79c3573535a2fed1a5e953fa92211a92b5fb0314
CRISTIAN <cjgc4002@gmail.com>
2024-04-09 21:51:43 -0300

    refactor: funcionario/ add funcionario static list

--- src/app/modules/funcionario/funcionario.component.ts
+++ src/app/modules/funcionario/funcionario.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-funcionario',
   templateUrl: './funcionario.component.html',
   styleUrls: ['./funcionario.component.css']
 })
-export class FuncionarioComponent {
+export class FuncionarioComponent implements OnInit {
 
+  constructor( ){
+
+  }
+
+  funcionarios  = [
+    {'id':1 , 'nome':'1Cristian'},
+    {'id':2 , 'nome':'2Cristian'},
+    {'id':3 , 'nome':'3Cristian'},
+    {'id':4 , 'nome':'4Cristian'},
+   ];
+
+  ngOnInit() {
+    this.funcionarios;
+    console.log(this.funcionarios)
+  }
 }
