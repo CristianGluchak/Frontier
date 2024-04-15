96d2a950fd97b274a5f0bbd7333f429accf578ee
CRISTIAN <cjgc4002@gmail.com>
2024-04-14 23:02:30 -0300

    Refactor: modifica fonte do funcionarios

--- src/app/modules/funcionario/funcionario.component.ts
+++ src/app/modules/funcionario/funcionario.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-funcionario',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FuncionarioComponent implements OnInit {
 
-  constructor( ){
+  constructor(
+    private router : Router
+   ){
 
   }
 
@@ -21,5 +24,9 @@ export class FuncionarioComponent implements OnInit {
   ngOnInit() {
     this.funcionarios;
     console.log(this.funcionarios)
+  }
+
+  goToDetalhes(id: number){
+    this.router.navigate(["/id"])
   }
 }
